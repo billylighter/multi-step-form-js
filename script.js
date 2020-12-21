299739681fb6cb469cf9081e8b61e9f849aa6857
Vladimir <vladimir.selevertov@gmail.com>
2020-12-21 23:23:23 +0200

    feature 2

--- script.js
+++ script.js
@@ -3,37 +3,27 @@ var current_fs, next_fs, previous_fs; //fieldsets
 var left, opacity, scale; //fieldset properties which we will animate
 var animating; //flag to prevent quick multi-click glitches
 var fieldsetLoadingClass = 'loading';
+const timingDelay = 1200;
 
-$(".next").click(function () {
+$('#login-register').on('click', () => {
+  $('.fieldset-steps').toggleClass('active');
+})
+
+$("#refresh-password-return-login").click(function () {
   if (animating) return false;
   animating = true;
-
   current_fs = $(this).parent();
-  next_fs = $(this).parent().next();
-
+  next_fs = $('#fieldset-steps-login #fieldset-login');
   $(current_fs).addClass(fieldsetLoadingClass);
-
-  //hide the current fieldset with style
   setTimeout(() => {
-    
-  //activate next step on progressbar using the index of next_fs
-  $("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
-
-  //show the next fieldset
-
   next_fs.show();
     current_fs.animate(
       { opacity: 0 },
       {
         step: function (now, mx) {
-          //as the opacity of current_fs reduces to 0 - stored in "now"
-          //1. scale current_fs down to 80%
-          scale = 1 - (1 - now) * 0.2;
-          
-          //2. bring next_fs from the right(50%)
-          left = now * 50 + "%";
-          
-          //3. increase opacity of next_fs to 1 as it moves in
+          scale = 1;
+          // left = now * 50 + "%";
+          left = 0;
           opacity = 1 - now;
           current_fs.css({
             transform: "scale(" + scale + ")",
@@ -46,39 +36,98 @@ $(".next").click(function () {
           current_fs.hide();
           animating = false;
         },
-        //this comes from the custom easing plugin
         easing: "easeInOutBack",
       }
     );
     $(current_fs).removeClass(fieldsetLoadingClass);
-  }, 1000);
+  }, timingDelay);
+});
+
+$("#refresh-password-link").click(function () {
+  if (animating) return false;
+  animating = true;
+  current_fs = $(this).parent();
+  next_fs = $('#fieldset-support #refresh-password-fieldset');
+  $(current_fs).addClass(fieldsetLoadingClass);
+  setTimeout(() => {
+  next_fs.show();
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now, mx) {
+          scale = 1;
+          // left = now * 5 + "%";
+          left = 0;
+          opacity = 1 - now;
+          current_fs.css({
+            transform: "scale(" + scale + ")",
+            position: "absolute",
+          });
+          next_fs.css({ left: left, opacity: opacity });
+        },
+        duration: 800,
+        complete: function () {
+          current_fs.hide();
+          animating = false;
+        },
+        easing: "easeInOutBack",
+      }
+    );
+    $(current_fs).removeClass(fieldsetLoadingClass);
+  }, timingDelay);
+});
+
+$(".next").click(function () {
+  if (animating) return false;
+  animating = true;
+  current_fs = $(this).parent();
+  next_fs = $(this).parent().next();
+  $(current_fs).addClass(fieldsetLoadingClass);
+  setTimeout(() => {
+  next_fs.show();
+    current_fs.animate(
+      { opacity: 0 },
+      {
+        step: function (now, mx) {
+          scale = 1;
+          // left = now * 5 + "%";
+          left = 0;
+          opacity = 1 - now;
+          current_fs.css({
+            transform: "scale(" + scale + ")",
+            position: "absolute",
+          });
+          next_fs.css({ left: left, opacity: opacity });
+        },
+        duration: 800,
+        complete: function () {
+          current_fs.hide();
+          animating = false;
+        },
+        easing: "easeInOutBack",
+      }
+    );
+    $(current_fs).removeClass(fieldsetLoadingClass);
+  }, timingDelay);
 });
 
 $(".previous").click(function () {
   if (animating) return false;
   animating = true;
-
   current_fs = $(this).parent();
   previous_fs = $(this).parent().prev();
-
-  //de-activate current step on progressbar
   $("#progressbar li")
     .eq($("fieldset").index(current_fs))
     .removeClass("active");
-
-  //show the previous fieldset
   previous_fs.show();
-  //hide the current fieldset with style
   current_fs.animate(
     { opacity: 0 },
     {
       step: function (now, mx) {
-        //as the opacity of current_fs reduces to 0 - stored in "now"
-        //1. scale previous_fs from 80% to 100%
-        scale = 0.8 + (1 - now) * 0.2;
-        //2. take current_fs to the right(50%) - from 0%
-        left = (1 - now) * 50 + "%";
-        //3. increase opacity of previous_fs to 1 as it moves in
+        // scale = 0.8 + (1 - now) * 0.2;
+        // left = (1 - now) * 50 + "%";
+        scale = 1;
+        left = 0;
         opacity = 1 - now;
         current_fs.css({ left: left });
         previous_fs.css({
@@ -91,7 +140,6 @@ $(".previous").click(function () {
         current_fs.hide();
         animating = false;
       },
-      //this comes from the custom easing plugin
       easing: "easeInOutBack",
     }
   );
